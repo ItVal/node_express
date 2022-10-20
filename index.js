bf17f21a266303c7f18321036929ef333f7483d9
ItVal <valentinhonvj1@gmail.com>
2022-10-20 20:28:06 +0200

    view engine

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+app.set('view engine', 'ejs');
+app.set("views", "pages");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + `/IHM/Accueil.html`);
+    res.render(`Accueil`, {name:'ValNas'});
 });
 
 app.get("/accueil", (req, res) => {
-  res.sendFile(__dirname + `/IHM/Accueil.html`);
+  res.render(`Accueil`, {name:'ValNas'});
 });
 
 app.get("/profil", (req, res) => {
-  res.sendFile(__dirname + `/IHM/Profil.html`);
+  res.render(`Profil`, {name:'ValNas'});
 });
 app.get("/erreur", (req, res) => {
-  res.sendFile(__dirname + `/IHM/erreur.html`);
+  res.render(`erreur`, {name:'ValNas'});
 });
 
 
